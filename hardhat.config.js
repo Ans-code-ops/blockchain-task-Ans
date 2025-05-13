e3b1565445891d0d1a92382345b0def71360b019
Ans-code-ops <ansawann10@gmail.com>
2025-05-13 20:37:08 +0500

    Initial commit for Smart contract deposit withdrwa

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-// IMPORTANT: Replace "YourEtherscanApiKey" with your actual API key from https://etherscan.io/myapikey
-// You need to:
-// 1. Create an account on Etherscan if you don't have one
-// 2. Go to https://etherscan.io/myapikey
-// 3. Click "Add" to create a new API key
-// 4. Copy your API key and paste it below
-const ETHERSCAN_API_KEY = "JY3CG81Z63ZITREP8CY2SIBHCCXQ93SA24";
 
-// Hardcoded values for deployment
-const PRIVATE_KEY = "0x6d58f020b91a9cc03c2ea21f1ca9128c7c1e9f3df35583e4dbfb77a97c41f50f";
-const SEPOLIA_RPC_URL = "https://eth-sepolia.g.alchemy.com/v2/0KQ9pxKncjc0-09HR1q5JqES_CETOJFC";
+const ETHERSCAN_API_KEY = ETHERSCAN_API_KEY;
+
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 
 module.exports = {
   solidity: "0.8.29",
